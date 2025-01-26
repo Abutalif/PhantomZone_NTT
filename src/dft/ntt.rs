332891f5fd73a23d06512b9778eb48526624686d
Abutalif <abutaliph@gmail.com>
2025-01-27 03:20:43 +0500

    added unit tests

--- src/dft/ntt.rs
+++ src/dft/ntt.rs
@@ -184,3 +184,136 @@ impl Table<u64> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::thread_rng;
+
+    /// Tests if `a == INTT(NTT(a))` for a lazy calculation with known numbers.
+    #[test]
+    fn self_inverse_known_lazy() {
+        let q = 0x1fffffffffe00001u64;
+        let n = 16;
+        let ntt = NttBuilder::new(q, n).build().expect("Cannot build");
+    
+        // known element slice.
+        let mut a:Vec<u64> = (1..=n as u64).collect();
+        let a_clone = a.clone();
+
+        ntt.forward_inplace_lazy(&mut a);
+        ntt.backward_inplace_lazy(&mut a);
+
+        assert_eq!(a, a_clone);
+    }
+
+    /// Tests if `a == INTT(NTT(a))` for a greedy calculation with known numbers.
+    #[test]
+    fn self_inverse_known_greedy() {
+        let q = 0x1fffffffffe00001u64;
+        let n = 16;
+        let ntt = NttBuilder::new(q, n).build().expect("Cannot build");
+    
+        // known element slice.
+        let mut a:Vec<u64> = (1..=n as u64).collect();
+        let a_clone = a.clone();
+
+        ntt.forward_inplace(&mut a);
+        ntt.backward_inplace(&mut a);
+
+        assert_eq!(a, a_clone);
+    }
+
+    /// Tests if `a == INTT(NTT(a))` for a greedy calculation with random numbers.
+    #[test]
+    fn self_inverse_random_greedy() {
+        let q = 0x1fffffffffe00001u64;
+        let n = 16;
+        let ntt = NttBuilder::new(q, n).build().expect("Cannot build");
+
+        let mut rng = thread_rng();
+        let mut b = vec![0u64; n].into_boxed_slice();
+
+        for el in b.iter_mut() {
+            *el = rng.gen_range(0..q);
+        }
+
+        let b_clone = b.clone();
+        ntt.forward_inplace(&mut b);
+        ntt.backward_inplace(&mut b);
+
+        assert_eq!(b, b_clone);
+    }
+
+    /// Tests if `a == INTT(NTT(a))` for a lazy calculation with random numbers.
+    #[test]
+    fn self_inverse_random_lazy() {
+        let q = 0x1fffffffffe00001u64;
+        let n = 16;
+        let ntt = NttBuilder::new(q, n).build().expect("Cannot build");
+
+        let mut rng = thread_rng();
+        let mut b = vec![0u64; n].into_boxed_slice();
+
+        for el in b.iter_mut() {
+            *el = rng.gen_range(0..q);
+        }
+
+        let b_clone = b.clone();
+        ntt.forward_inplace_lazy(&mut b);
+        ntt.backward_inplace_lazy(&mut b);
+
+        assert_eq!(b, b_clone);
+    }
+
+    /// Checks if lazy polynomial multiplication result matches with manual calculation.
+    #[test]
+    fn polynomial_mul_known_lazy() {
+        let q = 7681;
+        let n = 4;
+        let ntt = NttBuilder::new(q, n).build().expect("Cannot build");
+        let mut a = vec![1u64,2,3,4];
+        let mut b = vec![5u64,6,7,8];
+        let manual = vec![7625u64, 7645, 2, 60];
+
+        
+        ntt.forward_inplace_lazy(&mut a);
+        ntt.forward_inplace_lazy(&mut b);
+
+        elementwise_mod_mul(&mut a, &mut b, q);
+        
+        ntt.backward_inplace_lazy(&mut a);
+
+        assert_eq!(manual, a);
+    }
+
+    /// Checks if greedy polynomial multiplication result matches with manual calculation.
+    #[test]
+    fn polynomial_mul_known_greedy() {
+        let q = 7681;
+        let n = 4;
+        let ntt = NttBuilder::new(q, n).build().expect("Cannot build");
+        let mut a = vec![1u64,2,3,4];
+        let mut b = vec![5u64,6,7,8];
+        let manual = vec![7625u64, 7645, 2, 60];
+        
+        ntt.forward_inplace(&mut a);
+        ntt.forward_inplace(&mut b);
+        
+        elementwise_mod_mul(&mut a, &mut b, q);
+
+        ntt.backward_inplace(&mut a);
+
+        assert_eq!(manual, a);
+    }
+
+    fn elementwise_mod_mul(a: &mut [u64], b: &mut [u64], q: u64) {
+        for i in 0..a.len() {
+            a[i] = ((a[i] as u128 * b[i] as u128) % q as u128) as u64;
+        }
+    }
+
+    fn manual_polynomial_mod_mul(){
+        // TODO
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,9 @@
-use app::dft::{ntt::NttBuilder, DFT};
-use num_traits::PrimInt;
-use rand::{thread_rng, Rng};
-
 fn main() {
-    // known value polynomials
-    let q = 7681;
-    let n = 4;
-    let ntt = NttBuilder::new(q, n).build().expect("Cannot build");
-    let mut a = vec![1u64,2,3,4];
-    let mut b = vec![5u64,6,7,8];
-    
-    ntt.forward_inplace(&mut a);
-    ntt.forward_inplace(&mut b);
-    
-    let mut c = vec!(0; n);
-    for i in 0..n {
-        c[i] = ((a[i] as u128 * b[i] as u128) % q as u128) as u64;
-    }
-    ntt.backward_inplace(&mut c);
-
-    println!("mine: {c:?}");
-
-
-    let q = 0x1fffffffffe00001u64;
-    let n = 16;
-    let ntt = NttBuilder::new(q, n).build().expect("Cannot build");
-
-    // known element slice.
-    let mut a:Vec<u64> = (1..=n as u64).collect();
-    let a_clone = a.clone();
-    println!("a = {a:?}");
-    ntt.forward_inplace(&mut a);
-    ntt.backward_inplace(&mut a);
-    println!("INTT(NTT(a)) = {a:?}"); // Ok, So far, this works.
-    assert_eq!(a, a_clone);
-
-    // random filled slice
-    let mut rng = thread_rng();
-    let mut b = vec![0u64; n].into_boxed_slice();
-
-    for el in b.iter_mut() {
-        *el = rng.gen_range(0..q);
-    }
-
-    let b_clone = b.clone();
-    println!("b = {b:?}");
-    ntt.forward_inplace(&mut b);
-    ntt.backward_inplace(&mut b);
-    println!("INTT(NTT(b)) = {b:?}"); // Ok, So far, this works.
-
-    assert_eq!(b, b_clone);
-
     // Special prime
     let spec_q:u64 = ((((1 << 63) - (1 << 31)))<<1) + 1;
     let spec_bit = 0xffffffff00000001u64;
 
     assert_eq!(spec_q, spec_bit);
 
-    println!("Special Q: {spec_q}; spocal Bit: {spec_bit}");
-
-
+    println!("Special Q: {spec_q}; special Bit: {spec_bit}");
 }
